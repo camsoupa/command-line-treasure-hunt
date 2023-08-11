8f975d48ccb79335d5e7a8f8217d31251f3a5405
camsoupa <camsoupa@users.noreply.github.com>
2023-08-11 15:36:27 -0400

    Update peek_into_hole.sh

--- garden/peek_into_hole.sh
+++ garden/peek_into_hole.sh
@@ -9,3 +9,14 @@ python open_image.py
 
 # OR just:
 xdg-open hole.jpg
+
+echo "Achievement unlocked! You've got serious guts."
+echo ""
+echo "By the way, this is a shell script run or executed by the bash program ('BASH' is short for Bourne Again SHell"
+echo "which was released in 1989 as the 'reborn' new version of the shell previously developed by Stephen Bourne in 1979)."
+echo "A shell is just a terminal from which you can run programs by typing the program name followed by a space "
+echo "then any customizing settings afterwards (in this case, the script file name for example 'bash peek_into_hole.sh')."
+echo "Shell scripts are often short programs that run other programs and combine the results."
+echo ""
+echo "FYI, Python scripts are often a great replacement for shell scripts!"
+
