24348dcb2de81382cf4241176b213e59761fe4d5
camsoupa <camsoupa@users.noreply.github.com>
2023-08-11 15:14:32 -0400

    Create open_treasure_chest.sh

---
+++ house/upstairs/bedroom/open_treasure_chest.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+# TODO: 
+
+echo "Empty!  Did someone (or something) beat you to it?!"
+echo "You notice what looks like the skin of a large snake on the floor to the side of the chest."
+echo "Hmmm..."
+echo "You notice a window behind the treasure chest.  To peek out the window, 
