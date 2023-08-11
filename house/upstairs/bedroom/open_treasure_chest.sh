b225dcead4e3eac5ddfab4c5552b71b2c6a4721b
camsoupa <camsoupa@users.noreply.github.com>
2023-08-11 15:41:10 -0400

    Update open_treasure_chest.sh

--- house/upstairs/bedroom/open_treasure_chest.sh
+++ house/upstairs/bedroom/open_treasure_chest.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-# TODO: 
-
 echo "Empty!  Did someone (or something) beat you to it?!"
 echo "You notice what looks like the skin of a large snake on the floor to the side of the chest."
 echo "Hmmm..."
-echo "You notice a window behind the treasure chest.  To peek out the window, 
+echo "You notice a window behind the treasure chest.  To peek out the window, run the command: python peek_out_window.py"
+echo ""
+echo "Notice that the command is alot like the other commands: the program name followed by what the program should operate on, in this case, a file ending in .py to convey that it is a python script."
